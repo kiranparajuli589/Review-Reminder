3ef030f9ec7e5fcbfc3e0d2e467ff64154bae7af
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-25 20:48:40 +0545

    used variables for username and gitaccesstoken

--- review_requested.sh
+++ review_requested.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "This is user ${USERNAME} with token ${GIT_ACCESS_TOKEN}"
 declare -a REQUEST_TIME_ARRAY
 REQUEST_TIME_ARRAY=(103000 163000)
 while true 
@@ -8,7 +8,7 @@ do
 	sleep 0.5
 	if [[ " ${REQUEST_TIME_ARRAY[@]} " =~ " $now " ]]
 	then
-		curl -u kiranparajuli589:00f3699c8f17539ce7c4af2a79603de6c4996b4a https://api.github.com/search/issues\?q\=is:pr+state:open+review-requested:kiranparajuli589+archived:false | jq ".total_count" > output.json
+		curl -u $USERNAME:$GIT_ACCESS_TOKEN https://api.github.com/search/issues\?q\=is:pr+state:open+review-requested:${USERNAME}+archived:false | jq ".total_count" > output.json
 		count=`cat output.json`
 		if [[ $count -gt 1 ]]
 		then
